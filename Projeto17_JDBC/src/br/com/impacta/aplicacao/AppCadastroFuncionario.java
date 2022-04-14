f1adab80100dcc4c3759bfaa0311feedaa213958
harleiaki <96266332+harleiaki@users.noreply.github.com>
2022-04-14 19:25:59 -0300

    Add files via upload

--- Projeto17_JDBC/src/br/com/impacta/aplicacao/AppCadastroFuncionario.java
+++ Projeto17_JDBC/src/br/com/impacta/aplicacao/AppCadastroFuncionario.java
@@ -1,10 +1,11 @@
 package br.com.impacta.aplicacao;
 
+import javax.swing.JOptionPane;
+import java.sql.Connection;
 import java.sql.DriverManager;
 
-import javax.swing.JOptionPane;
 
-import com.mysql.jdbc.Connection;
+
 
 public class AppCadastroFuncionario {
 	
@@ -12,11 +13,14 @@ public class AppCadastroFuncionario {
 	
 		try {
 			
-			String conexao = "jdbc:mysql://localhost:3306/db_cadastro?autoSSL=false";
+			String conexao = 
+					"jdbc:mysql://localhost:3306/db_cadastro?autoSSL=false";
 			
-			Class.forName("com.mysql.jdbc.Driver");
+			Class.forName
+					("com.mysql.jdbc.Driver");
 			
-			Connection cn = (Connection)DriverManager.getConnection(conexao,"root", "Imp@ct@");
+			Connection cn = 
+					(Connection)DriverManager.getConnection(conexao,"root", "Imp@ct@");
 			
 			JOptionPane.showMessageDialog(null, "Conexão Ok");
 			
