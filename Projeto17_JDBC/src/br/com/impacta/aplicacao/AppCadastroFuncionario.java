0f1d0c35a977f342d81e98ac58d5dd973aa60138
harleiaki <96266332+harleiaki@users.noreply.github.com>
2022-04-14 20:14:05 -0300

    Add files via upload

--- Projeto17_JDBC/src/br/com/impacta/aplicacao/AppCadastroFuncionario.java
+++ Projeto17_JDBC/src/br/com/impacta/aplicacao/AppCadastroFuncionario.java
@@ -1,10 +1,9 @@
 package br.com.impacta.aplicacao;
 
-import javax.swing.JOptionPane;
 import java.sql.Connection;
 import java.sql.DriverManager;
-
-
+import java.sql.PreparedStatement;
+import javax.swing.JOptionPane;
 
 
 public class AppCadastroFuncionario {
@@ -22,18 +21,41 @@ public class AppCadastroFuncionario {
 			Connection cn = 
 					(Connection)DriverManager.getConnection(conexao,"root", "Imp@ct@");
 			
-			JOptionPane.showMessageDialog(null, "Conexão Ok");
+			
+			//incluindo um novo funcionário
+			String sql = "INSERT INTO TBFUNCIONARIOS(DOCUMENTO, NOME, IDADE," + 
+							"SEXO, CARGO, SALARIO) VALUES (?,?,?,?,?,?)";
 			
 			
+			String documento = 
+					JOptionPane.showInputDialog("Informe os documentos:");
+			String nome = 
+					JOptionPane.showInputDialog("Informe o nome:");
+			int idade = Integer.parseInt(
+					JOptionPane.showInputDialog("Informe a idade"));
+			String sexo = 
+					JOptionPane.showInputDialog("Informe o sexo (M ou F):");
+			String cargo = 
+					JOptionPane.showInputDialog("Informe o cargo:");
+			double salario = Double.parseDouble(
+					JOptionPane.showInputDialog("Informe o salário:"));
+			
+			
+			PreparedStatement stmt = cn.prepareStatement(sql);
+			stmt.setString(1,documento);
+			stmt.setString(2,nome);
+			stmt.setInt(3,idade);
+			stmt.setString(4,sexo);
+			stmt.setString(5,cargo);
+			stmt.setDouble(6,salario);
+			
+			stmt.executeUpdate();
+			JOptionPane.showMessageDialog(null, "Funcionário incluído com sucesso!");
+	
 			
 			
 		} catch (Exception e) {
 			e.printStackTrace();
-		}
-		
-		
-		
-		
+		}		
 	}
-
 }
---
+++ Projeto17_JDBC/src/br/com/impacta/aplicacao/AppConsultaFuncionarios.java
@@ -0,0 +1,31 @@
+package br.com.impacta.aplicacao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+public class AppConsultaFuncionarios {
+	public static void main(String[] args) {
+		
+		try {
+			
+			String conexao = 
+					"jdbc:mysql://localhost:3306/db_cadastro?autoSSL=false";
+			
+			Class.forName
+					("com.mysql.jdbc.Driver");
+			
+			Connection cn = 
+					(Connection)DriverManager.getConnection(conexao,"root", "Imp@ct@");
+			
+			String sql = "SELECT * FROM TBFUNCIONARIOS";
+			
+			PreparedStatement stmt = cn.prepareCall(sql);
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
